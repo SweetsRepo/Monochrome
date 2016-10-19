9b55a20838fb3b81eac7ceb5fbd97f078bd0aafa
Christopher Sweet <crs4263@rit.edu>
2016-10-18 20:28:40 -0400

    Added in enumeration for unit costs

--- src/engine/buildings/Barracks.java
+++ src/engine/buildings/Barracks.java
@@ -9,7 +9,19 @@ import engine.units.Unit;
  */
 public class Barracks {
 
-	public static enum UnitCosts{};
+	/**
+	 * Enumeration of all the units and their production cost
+	 */
+	public static enum UnitCosts{ SCOUT(100), WORKER(200), HEAVY(300), SUPPORT(400), GRENADIER(500), 
+		TURRET(600), LONGRANGE(700), MEDIC(800), ANGELOFDEATH(900), SWARM(1000), HARBINGER(1100); 
+		private int cost;
+		UnitCosts(int cost){
+			this.cost = cost;
+		}
+		public int getCost(){
+			return this.cost;
+		}
+	};
 	
 	public Unit constructUnit(int resourcesAvail, int resourcesCost){
 		if(resourcesAvail >= resourcesCost)
